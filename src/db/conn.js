00ff367df83ade20423e597a611bb9c0b80fcf3c
jayantmanapure1 <jayantmanapure1@gmail.com>
2023-07-25 15:10:48 +0530

    add .env1

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect("mongodb+srv://jayantmanapure5:ekQamG55zP3tuNW1@cluster0.40vzaaa.mongodb.net/")
 .then(()=>{
    console.log("connection is Successfully")
 }).catch((e)=>{
